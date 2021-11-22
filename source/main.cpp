0655595549bdaecaef1163f32ce2c002d29d4e8c
Konstantin <konstantin.seurer@gmail.com>
2021-11-22 13:19:22 +0100

    Added CLI

--- source/main.cpp
+++ source/main.cpp
@@ -1,85 +1,203 @@
 
 #include <iostream>
 #include <filesystem>
-#include <fstream>
 
 #include "Base.h"
-#include "Lexer.h"
 #include "BuildContext.h"
 #include "Time.h"
 
-static void CompileSourceFile(const String &filename)
+enum class Flag
 {
-	if (std::filesystem::is_directory(filename))
-	{
-		for (const auto &file : std::filesystem::directory_iterator(filename))
-		{
-			CompileSourceFile(file.path());
-		}
+	HELP,
+	MODULE_PATH,
+	CLEAN,
+	OUTPUT_PATH,
+	OPTIMIZATION_LEVEL,
+	COUNT,
+};
 
-		return;
+struct FlagData
+{
+public:
+	char shortName;
+	String longName;
+	String description;
+	Flag flag;
+	bool hasValue;
+
+public:
+	FlagData(char shortName, String longName, String description, Flag flag, bool hasValue)
+		: shortName(shortName), longName(longName), description(description), flag(flag), hasValue(hasValue)
+	{
+	}
+};
+
+static FlagData flagData[(UInt64)Flag::COUNT] = {
+	{'h', "help", "Print this help output.", Flag::HELP, false},
+	{'p', "module-path", "Semicolon seperated list of every caconical path that contains modules. (-p /path/to/module)", Flag::MODULE_PATH, true},
+	{'c', "clean", "Clear the output directory before building.", Flag::CLEAN, false},
+	{'o', "output-path", "Directory where the binaries and cache files will be written to. (-o /path/to/output)", Flag::OUTPUT_PATH, true},
+	{'O', "optimization-level", "Indicates what optimizations should be done. Can be debug, performance or size. (-O size)", Flag::OPTIMIZATION_LEVEL, true}};
+
+static void PrintHelp()
+{
+	std::cout << "Usage:" << std::endl;
+	std::cout << "\tstrictllvm [flags...] modules..." << std::endl;
+	std::cout << std::endl;
+	std::cout << "Flags:" << std::endl;
+
+	UInt64 maxLongNameLength = 0;
+	for (const auto &flag : flagData)
+	{
+		if (maxLongNameLength < flag.longName.length())
+		{
+			maxLongNameLength = flag.longName.length();
+		}
 	}
 
-	if (filename.length() < 7 && filename.substr(filename.length() - 7, 7) != ".strict")
+	for (const auto &flag : flagData)
 	{
-		return;
-	}
+		std::cout << "\t-" << flag.shortName << " --" << flag.longName;
 
-	std::cout << "\tBuilding source file '" << filename << "'..." << std::endl;
-
-	std::ifstream fileStream(filename);
-	String fileContent = String(std::istreambuf_iterator<char>(fileStream), std::istreambuf_iterator<char>());
-	Ref<TokenStream> tokens = TokenStream::Create(fileContent);
-
-	do
-	{
-		Token token = tokens->Next();
-
-		std::cout << "\t\t" << ToString(token.type);
-
-		switch (token.type)
+		const UInt64 spaceCount = maxLongNameLength - flag.longName.length() + 1;
+		for (UInt64 spaceIndex = 0; spaceIndex < spaceCount; spaceIndex++)
 		{
-		case TokenType::STRING_LITERAL:
-			std::cout << " \"" << token.data.stringData << "\"";
-			break;
-		case TokenType::INT_LITERAL:
-			std::cout << " " << token.data.intData;
-			break;
-		case TokenType::UINT_LITERAL:
-			std::cout << " " << token.data.uintData;
-			break;
-		case TokenType::FLOAT_LITERAL:
-			std::cout << " " << token.data.floatData;
-			break;
-		case TokenType::IDENTIFIER:
-			std::cout << " " << token.data.stringData;
-			break;
+			std::cout << " ";
 		}
+		std::cout << flag.description << std::endl;
+	}
+}
 
-		std::cout << std::endl;
-	} while (tokens->HasNext());
+void Split(const String &string, const char delim, Array<String> &out)
+{
+	size_t start;
+	size_t end = 0;
+
+	while ((start = string.find_first_not_of(delim, end)) != String::npos)
+	{
+		end = string.find(delim, start);
+		out.push_back(string.substr(start, end - start));
+	}
 }
 
 int main(int argc, const char **args)
 {
 	if (argc < 2)
 	{
+		PrintHelp();
 		return 0;
 	}
 
-	std::cout << "Building module '" << args[1] << "'..." << std::endl;
+	Array<Pair<Flag, String>> valueFlags;
+	Array<Flag> flags;
+	Array<String> moduleNames;
 
-	const Array<String> modulePath = {std::filesystem::current_path()};
-	const String cachePath = String(std::filesystem::current_path()) + "/cache";
+	UInt64 flagValueIndex = 0;
+	for (UInt64 argumentIndex = 1; argumentIndex < argc; argumentIndex++)
+	{
+		const char *argument = args[argumentIndex];
+
+		if (argument[0] == '-')
+		{
+			if (argument[1] == '-')
+			{
+				const char *argumentFlag = &(argument[2]);
+
+				Bool found = false;
+				for (const auto &flag : flagData)
+				{
+					if (flag.longName == argumentFlag)
+					{
+						if (flag.hasValue)
+						{
+							valueFlags.emplace_back(flag.flag, "");
+						}
+						else
+						{
+							flags.emplace_back(flag.flag);
+						}
+						found = true;
+						break;
+					}
+				}
+
+				if (!found)
+				{
+					std::cerr << "Undefines argument flag '" << argumentFlag << "'!" << std::endl;
+				}
+			}
+			else
+			{
+				for (UInt64 characterIndex = 1; argument[characterIndex] != '\0'; characterIndex++)
+				{
+					Bool found = false;
+					for (const auto &flag : flagData)
+					{
+						if (flag.shortName == argument[characterIndex])
+						{
+							if (flag.hasValue)
+							{
+								valueFlags.emplace_back(flag.flag, "");
+							}
+							else
+							{
+								flags.emplace_back(flag.flag);
+							}
+							found = true;
+							break;
+						}
+					}
+
+					if (!found)
+					{
+						std::cerr << "Undefines argument flag '" << argument[characterIndex] << "'!" << std::endl;
+					}
+				}
+			}
+			continue;
+		}
+
+		if (flagValueIndex < valueFlags.size())
+		{
+			valueFlags[flagValueIndex].second = argument;
+			flagValueIndex++;
+			continue;
+		}
+
+		moduleNames.push_back(argument);
+	}
+
+	Array<String> modulePath;
+	for (const auto &flag : valueFlags)
+	{
+		if (flag.first == Flag::MODULE_PATH)
+		{
+			Split(flag.second, ';', modulePath);
+			break;
+		}
+	}
+
+	String outputPath = String(std::filesystem::current_path()) + "/output";
+	for (const auto &flag : valueFlags)
+	{
+		if (flag.first == Flag::OUTPUT_PATH)
+		{
+			outputPath = flag.second;
+			break;
+		}
+	}
 
 	const TargetFlags target = TargetFlags::BIT64 | TargetFlags::LINUX | TargetFlags::X86;
 
-	BuildContext context(modulePath, cachePath, target);
+	BuildContext context(modulePath, outputPath, target);
 
 	std::cout << "Scanning modules...";
 	Time scanStart;
 
-	context.AddModule(args[1]);
+	for (const String &moduleName : moduleNames)
+	{
+		context.AddModule(moduleName);
+	}
 
 	std::cout.precision(1);
 	std::cout << " (" << (Time() - scanStart).milliSeconds() << "ms)" << std::endl;
